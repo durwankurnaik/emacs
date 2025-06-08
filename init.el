c05bf178500d2ae59bcd01aa0c72116d355f827e
Durwankur Naik <ndurwankur14@gmail.com>
2025-06-08 19:41:41 +0530

    updated font to firacode nerd font

--- init.el
+++ init.el
@@ -17,8 +17,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(dmenu exwm spacemacs-theme afternoon-theme afternoon evil-collection evil undo-fu writeroom-mode which-key vterm use-package toc-org switch-window sudo-edit spaceline smex rainbow-mode rainbow-delimiters projectile org-bullets neotree magit ivy-rich ivy-posframe ido-vertical-mode expand-region emojify doom-modeline dired-launch diminish dashboard cyberpunk-theme counsel company async all-the-icons-ibuffer all-the-icons-dired)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
